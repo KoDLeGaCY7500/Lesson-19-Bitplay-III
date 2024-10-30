f25e1b38c2cfcb06bc43746d0516b4eb5f293621
KoDleGaCY7500 <notkodlegacy7500@gmail.com>
2024-10-30 05:24:57 -0700

    activity 2

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-def swap(a,b):
-    a=a^b
-    b=a^b
-    a=a^b
-    print("after swapping a======",a,"b=======>",b)
+# def swap(a,b):
+#     a=a^b
+#     b=a^b
+#     a=a^b
+#     print("after swapping a======",a,"b=======>",b)
 
-swap(4,8)
+# swap(4,8)
 
-a=8
-b=4
+# a=8
+# b=4
 
-print("Before swapping===========>",a,b)
-a=a+b
-b=a-b
-a=a-b
+# print("Before swapping===========>",a,b)
+# a=a+b
+# b=a-b
+# a=a-b
 
-print("After swapping")
-print(a,b)
+# print("After swapping")
+# print(a,b)
 
-def swap(a,b):
-    a=(a&b)+(a|b)
-    b=a+(~b)+1
-    a=a+(~b)+1
+# def swap(a,b):
+#     a=(a&b)+(a|b)
+#     b=a+(~b)+1
+#     a=a+(~b)+1
 
-    print("numbers after swapping",a,b)
+#     print("numbers after swapping",a,b)
 
-swap(8,4)
+# swap(8,4)
+
+def divide(dividend,divisor):
+    sign=(-1 if((dividend<0)^(divisor<0))else 1);
+    dividend=abs(dividend);
+    divisor=abs(divisor);
+
+    quotientno=0
+    tempno=0
+    for i in range(31,-1,-1):
+        if (tempno+(divisor<<i) <= dividend):
+            tempno+=divisor<<i
+            quotientno|=1 <<i
+    if sign == -1:
+       quotientno=-quotientno
+    return quotientno
+
+a=int(input("Enter a for a/b: "))
+b=int(input("Enter b for b/a: "))
+print("Result of",a,"/",b,"is",divide(a,b))
